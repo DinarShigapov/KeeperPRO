d6fc0055e30bcca35efd3c8fd678fb8a638bef39
Dinar <dinar@DESKTOP-VO9N10F>
2023-06-01 23:10:11 +0300

    Добавьте файлы проекта.

---
+++ RegisterUnitCase/RegisterPageTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KeeperPRO.Pages.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeeperPRO.ADO;
+
+namespace KeeperPRO.Pages.Auth.Tests
+{
+    [TestClass()]
+    public class RegisterPageTests
+    {
+        [TestMethod()]
+        public void RegisterPageTest()
+        {
+            KeeperPRODBEntities DB = new KeeperPRODBEntities();
+
+            var emailInput = "keeper@mail.ru";
+            
+
+
+            Assert.IsTrue();
+        }
+
+
+    }
+}
