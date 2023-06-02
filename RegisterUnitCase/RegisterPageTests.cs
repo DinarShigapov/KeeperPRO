9c7ad45f61605c737b7a2f1669abba6a5e26a971
Fl4son <dinarhertz@gmail.com>
2023-06-02 13:58:03 +0300

    2 UnitCase

--- RegisterUnitCase/RegisterPageTests.cs
+++ RegisterUnitCase/RegisterPageTests.cs
@@ -14,7 +14,7 @@ namespace KeeperPRO.Pages.Auth.Tests
     public class RegisterPageTests
     {
         [TestMethod()]
-        public void RegisterPageTest()
+        public void RegisterSaveDBTest()
         {
             User newUser = new User
             {
@@ -33,6 +33,74 @@ namespace KeeperPRO.Pages.Auth.Tests
             Assert.IsTrue(newUser.ID != 0);
         }
 
+
+
+        [TestMethod()]
+        public void RegisterValidateDBTest()
+        {
+
+            string password = "Zz8POQlP}M4~";
+
+            if (!IsValidatePassword(password))
+            {
+                Assert.IsTrue(IsValidatePassword(password));
+                return;
+            }
+            
+
+            User newUser = new User
+            {
+                Email = "test2Email@gmail.com",
+                EncPassword = SHA512(password),
+                RoleCode = 1,
+                FirstName = "Зиновьева",
+                LastName = "Таисия",
+                SurName = "Яковна",
+                Login = "test2Email"
+            };
+
+            DBConnections.DB.User.Add(newUser);
+            DBConnections.DB.SaveChanges();
+
+            Assert.IsTrue(newUser.ID != 0);
+        }
+
+
+
+        private static bool IsValidatePassword(string password)
+        {
+            string message = "";
+            if (password.Length < 8)
+            {
+                message += "Пароль должен иметь как минимум 8 символов!\n";
+            }
+            if (!password.Any(x => char.IsUpper(x)))
+            {
+                message += "Пароль должен иметь как минимум 1 символ верхнего регистра!\n";
+            }
+            if (!password.Any(x => char.IsLower(x)))
+            {
+                message += "Пароль должен иметь как минимум 1 символ нижнего регистра!\n";
+            }
+            if (!password.Any(x => char.IsDigit(x)))
+            {
+                message += "Пароль должен иметь как минимум 1 цифру!\n";
+            }
+            List<char> specialSymbols = new List<char> {'!','@','#','№','$','%','^',
+                '&','*','-','_','?','<','>'};
+            if (password.Any(x => specialSymbols.Any(y => x == y)))
+            {
+                message += "Пароль должен иметь как минимум 1 спецсимвол!\n";
+            }
+            if (message != "")
+            {
+                Assert.Fail(message);
+                return false;
+            }
+            return true;
+        }
+
+
         public static string SHA512(string input)
         {
             var bytes = System.Text.Encoding.UTF8.GetBytes(input);
@@ -46,7 +114,6 @@ namespace KeeperPRO.Pages.Auth.Tests
                 return hashedInputStringBuilder.ToString();
             }
         }
-
 
     }
 }
